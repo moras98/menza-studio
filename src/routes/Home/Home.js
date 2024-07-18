506a06aed18b6769152cc0f0d87d56358619b49e
Agustin Moras <72901534+moras98@users.noreply.github.com>
2024-07-18 11:37:12 -0300

    home layout

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -6,24 +6,41 @@ import Filter from "../../elements/Filter";
 import HeroContent from "./Sections/HeroContent";
 import AboutContent from "./Sections/AboutContent";
 import ServicesContent from "./Sections/ServicesContent";
+import ClientsContent from "./Sections/ClientsContent";
+import ContactContent from "./Sections/ContactContent";
 
 export default function Home (){
     return(
         <>
-        <ParallaxSection img={'./assets/images/hero-section.jpg'} isHalfHeight={false}>
-            <HeroContent/>
-        </ParallaxSection>
-        <Section backgroundImage={`url('./assets/images/about-section.jpg')`}>
-            <SectionContent centered={true}>
-                <AboutContent/>
-            </SectionContent>
-            <Filter isBlue={false}/>
-        </Section>
-        <Section>
-            <SectionContent centered={true}>
-                <ServicesContent/>
-            </SectionContent>
-        </Section>
+            {/* HERO SECTION */}
+            <ParallaxSection img={'./assets/images/hero-section.jpg'} isHalfHeight={false}>
+                <HeroContent/>
+            </ParallaxSection>
+            {/* ABOUT SECTION */}
+            <Section backgroundImage={`url('./assets/images/about-section.jpg')`}>
+                <SectionContent centered={true}>
+                    <AboutContent/>
+                </SectionContent>
+                <Filter isBlue={false}/>
+            </Section>
+            {/* SERVICES SECTION */}
+            <Section>
+                <SectionContent centered={true}>
+                    <ServicesContent/>
+                </SectionContent>
+            </Section>
+            {/* CLIENTS SECTION */}
+            <Section>
+                <SectionContent centered={true}>
+                    <ClientsContent/>
+                </SectionContent>
+            </Section>
+            {/* CONTACT SECTION */}
+            <Section>
+                <SectionContent centered={true}>
+                    <ContactContent/>
+                </SectionContent>
+            </Section>
         </>
     );
 }
