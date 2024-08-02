43c2612a9a975a35745af677d3edaf3cb0ffe08b
Agustin Moras <72901534+moras98@users.noreply.github.com>
2024-08-02 18:49:15 -0300

    changes modal and home

--- src/components/ServicesGrid/ServicesElement.js
+++ src/components/ServicesGrid/ServicesElement.js
@@ -1,15 +1,15 @@
 import React from "react";
-import './ServicesElement.css';
+import "./ServicesElement.css";
 import ItalicButton from "../../elements/ItalicButton/ItalicButton";
 import { useTranslation } from "react-i18next";
 
-export default function ServicesElement({title, text, path}){
-    const {t} = useTranslation('home');
-    return(
-        <div className="services-element">
-            <h4>{title.toUpperCase()}</h4>
-            <p>{text}</p>
-            <ItalicButton path={path} string={t('about_btn')}/>
-        </div>
-    );
+export default function ServicesElement({ title, text, path }) {
+  const { t } = useTranslation("home");
+  return (
+    <div className="services-element">
+      <h4>{title.toUpperCase()}</h4>
+      {/* <p>{text}</p> */}
+      <ItalicButton path={path} string={t("about_btn")} />
+    </div>
+  );
 }
--- src/components/TeamMemberCV/TeamMemberCV.js
+++ src/components/TeamMemberCV/TeamMemberCV.js
@@ -1,40 +1,48 @@
 import React from "react";
-import './TeamMemberCV.css';
+import "./TeamMemberCV.css";
 import { useTranslation } from "react-i18next";
 
-export default function TeamMemberCV({teamMember}){
-    const {i18n} = useTranslation();
-    const currentLanguage = i18n.language;
-    return(
-        <div className="team-member-cv">
-            <div className="left">
-                <div className='img-container'>
-                    <img alt={teamMember.name} src={teamMember.src}/>
-                </div>
-                <h4>{teamMember.name}</h4>
-                <h5>{teamMember.position}</h5>
-                <div className="links">
-                    <img alt="email logo" src="/menza-studio/assets/icons/email.svg"/>
-                    <img alt="phone logo" src="/menza-studio/assets/icons/phone.svg"/>
-                    <img alt="linkedin logo" src="/menza-studio/assets/icons/linkedin.svg"/>
-                </div>
-                <p className="curriculum-link">{currentLanguage === 'es'? 'Currículum Completo' : 'Complete Curriculum'}</p>
-            </div>
-            <div className="right">
-                {/* Contenido a la derecha scrolleable con experiencia */}
-                <p>{teamMember.description}</p>
-                {teamMember.cv.map((e, idx)=> <CVRow row={e} key={idx}/>)}
-            </div>
+export default function TeamMemberCV({ teamMember }) {
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+  return (
+    <div className="team-member-cv">
+      <div className="left">
+        <div className="img-container">
+          <img alt={teamMember.name} src={teamMember.src} />
         </div>
-    );
+        <div>
+          <h4>{teamMember.name}</h4>
+          <h5>{teamMember.position}</h5>
+          <div className="links">
+            <img alt="email logo" src="/menza-studio/assets/icons/email.svg" />
+            <img alt="phone logo" src="/menza-studio/assets/icons/phone.svg" />
+            <img
+              alt="linkedin logo"
+              src="/menza-studio/assets/icons/linkedin.svg"
+            />
+          </div>
+          <p className="curriculum-link">
+            {currentLanguage === "es"
+              ? "Currículum Completo"
+              : "Complete Curriculum"}
+          </p>
+        </div>
+      </div>
+      <div className="right">
+        {/* Contenido a la derecha scrolleable con experiencia */}
+        <h4>{teamMember.description}</h4>
+        {/* {teamMember.cv.map((e, idx)=> <CVRow row={e} key={idx}/>)} */}
+      </div>
+    </div>
+  );
 }
 
-
-function CVRow({row}){
-    return(
-        <div className="cv-row">
-            <h5>{row.title}</h5>
-            <p>{row.description}</p>
-        </div>
-    );
-}
+// function CVRow({ row }) {
+//   return (
+//     <div className="cv-row">
+//       <h5>{row.title}</h5>
+//       <p>{row.description}</p>
+//     </div>
+//   );
+// }
