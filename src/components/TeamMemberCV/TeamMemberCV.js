48955c3d03cfbf04038663baa0332b7e6027f6e3
Agustin Moras <a.morascastro@gmail.com>
2024-09-15 17:05:39 -0300

    links de contanto funcionales

--- src/components/TeamMemberCV/TeamMemberCV.js
+++ src/components/TeamMemberCV/TeamMemberCV.js
@@ -5,6 +5,25 @@ import { useTranslation } from "react-i18next";
 export default function TeamMemberCV({ teamMember }) {
   const { i18n } = useTranslation();
   const currentLanguage = i18n.language;
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            currentLanguage === 'es' ? alert('Teléfono copiado!') : alert('Phone copied!');
+        })
+        .catch((err) => {
+            console.error('Error al copiar al portapapeles: ', err);
+        });
+  };
+
+  const handleEmailClick = (email) => {
+    window.location.href = `mailto:${email}`;
+  };
+
+  const handlePhoneClick = (phone) => {
+    copyToClipboard(phone);
+  };
+
   return (
     <div className="team-member-cv">
       <div className="left">
@@ -15,11 +34,20 @@ export default function TeamMemberCV({ teamMember }) {
           <h4>{teamMember.name}</h4>
           <h5>{teamMember.position}</h5>
           <div className="links">
-            <img alt="email logo" src="/menza-studio/assets/icons/email.svg" />
-            <img alt="phone logo" src="/menza-studio/assets/icons/phone.svg" />
-            <img
-              alt="linkedin logo"
-              src="/menza-studio/assets/icons/linkedin.svg"
+            <img 
+              alt="email logo" 
+              src="/menza-studio/assets/icons/email.svg" 
+              onClick={() => handleEmailClick(teamMember.email)} 
+            />
+            <img 
+              alt="phone logo" 
+              src="/menza-studio/assets/icons/phone.svg" 
+              onClick={() => handlePhoneClick(teamMember.phone)} // Asegúrate de que teamMember.phone esté definido
+            />
+            <img 
+              alt="linkedin logo" 
+              src="/menza-studio/assets/icons/linkedin.svg" 
+              onClick={() => window.open(teamMember.linkedin, '_blank')} // Añadido para manejar el clic en LinkedIn
             />
           </div>
           <p className="curriculum-link">
@@ -30,19 +58,8 @@ export default function TeamMemberCV({ teamMember }) {
         </div>
       </div>
       <div className="right">
-        {/* Contenido a la derecha scrolleable con experiencia */}
         <h4>{teamMember.description}</h4>
-        {/* {teamMember.cv.map((e, idx)=> <CVRow row={e} key={idx}/>)} */}
       </div>
     </div>
   );
 }
-
-// function CVRow({ row }) {
-//   return (
-//     <div className="cv-row">
-//       <h5>{row.title}</h5>
-//       <p>{row.description}</p>
-//     </div>
-//   );
-// }
--- src/routes/Contact/Contact.js
+++ src/routes/Contact/Contact.js
@@ -7,7 +7,20 @@ import UnderlinedTitle from "../../components/UnderlinedTitle/UnderlinedTitle";
 import { useTranslation } from "react-i18next";
 
 export default function Contact(){
-    const {t} = useTranslation('contact');
+    const {i18n, t} = useTranslation('contact');
+    const currentLanguage = i18n.language;
+
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                // Optional: show a message to indicate that the text has been copied
+                currentLanguage === 'es' ? alert('Teléfono copiado!') : alert('Phone copied!');
+            })
+            .catch((err) => {
+                console.error('Error al copiar al portapapeles: ', err);
+            });
+    };
+
     return(
         <>
             <ParallaxSection isHalfHeight={true} img={'/menza-studio/assets/images/hero-section.jpg'}>
@@ -20,33 +33,27 @@ export default function Contact(){
                             <UnderlinedTitle><h3 style={{color:'white'}}>{t('subtitle')}</h3></UnderlinedTitle>
                         </div>
                         <div className="right">
-                            <div className="contact-info">
+                            <div className="contact-info" onClick={()=>window.location.href = 'mailto:example@example.com'}>
                                 <div className="contacts">
                                     <p>Email</p>
                                 </div>
                                 <div className="contacts">
                                     <img alt="email" src="/menza-studio/assets/icons/email.svg"/>
-                                    {/* <img alt="email" src="/menza-studio/assets/icons/phone.svg"/> */}
-                                    {/* <img alt="email" src="/menza-studio/assets/icons/linkedin.svg"/> */}
                                 </div>  
                             </div>
-                            <div className="contact-info">
+                            <div className="contact-info" onClick={() => copyToClipboard('+1234567890')}>
                                 <div className="contacts">
                                     <p>Teléfono</p>
                                 </div>
                                 <div className="contacts">
-                                    {/* <img alt="email" src="/menza-studio/assets/icons/email.svg"/> */}
-                                    <img alt="email" src="/menza-studio/assets/icons/phone.svg"/>
-                                    {/* <img alt="email" src="/menza-studio/assets/icons/linkedin.svg"/> */}
+                                    <img alt="email" src="/menza-studio/assets/icons/phone.svg"/>                                
                                 </div>  
                             </div>
-                            <div className="contact-info">
+                            <div className="contact-info" onClick={()=> window.open('https://www.linkedin.com/company/sequeira-menza-abogados?trk=public_profile_topcard-current-company', '_blank')}>
                                 <div className="contacts">
                                     <p>LinkedIn</p>
                                 </div>
                                 <div className="contacts">
-                                    {/* <img alt="email" src="/menza-studio/assets/icons/email.svg"/> */}
-                                    {/* <img alt="email" src="/menza-studio/assets/icons/phone.svg"/> */}
                                     <img alt="email" src="/menza-studio/assets/icons/linkedin.svg"/>
                                 </div>  
                             </div>
